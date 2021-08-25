86253c3d731d61d48cc280aa10545f15b245be1e
Fabio Nascimento <fabiogtr@hotmail.com>
2021-08-25 10:40:58 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -22,6 +22,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -29,32 +33,39 @@ export class ProdutosPage {
         this.items = response['content'];
         loader.dismiss();
         this.loadImageUrls();
-      },
+    },
       error => {
         loader.dismiss();
       });
-    }
+  }
 
-    loadImageUrls() {
-      for (var i=0; i<this.items.length; i++) {
-        let item = this.items[i];
-        this.produtoService.getSmallImageFromBucket(item.id)
-          .subscribe(response => {
-            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-          },
-          error =>{});
-      }
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error =>{});
     }
+  }
 
-    showDetail(produto_id: string) {
-      this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
-    }
+  showDetail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
 
-    presentLoading() {
-      let loader = this.loadingCtrl.create({
-        content: "Aguarde..."
-      });
-      loader.present();
-      return loader;
-    }
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
 }
