f7c12e86d42fd6dac174a5954ce16f97f6d959be
Fabio Nascimento <fabiogtr@hotmail.com>
2021-08-30 14:06:56 -0300

    Bugfix - cancelamento da câmera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -70,6 +70,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
@@ -89,6 +90,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
